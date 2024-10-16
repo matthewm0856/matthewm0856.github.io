c490b19c67361f36ba8eeb0d219aa4b5b4fadf89
matthewm0856 <matthewm652@nycstudents.net>
2024-10-16 18:28:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Matthew Maias"; // update the part BETWEEN the "quotes"
+let userName = "matthewm0856"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
